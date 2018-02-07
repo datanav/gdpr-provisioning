a311599fd2ca29e8644ae15d0f00fbad3edbfa36
Ioannis Skoulis <ioannis.skoulis@sesam.io>
2018-02-07 16:00:18 +0100

    Minor updates

--- provision.sh
+++ provision.sh
@@ -16,9 +16,77 @@ if [ -z "$SUBSCRIPTION_ID" ]; then
   fi
 fi
 
-export DEBIAN_FRONTEND=noninteractive
-export DATABROWSER_DOCKER_IMAGE_TAG=${DATABROWSER_DOCKER_IMAGE_TAG:-prototype-encrypted-fields}
-export SOLR_DOCKER_IMAGE_TAG=${SOLR_DOCKER_IMAGE_TAG:-gdpr}
+DATABROWSER_DOCKER_IMAGE_TAG=${DATABROWSER_DOCKER_IMAGE_TAG:-prototype-encrypted-fields}
+SOLR_DOCKER_IMAGE_TAG=${SOLR_DOCKER_IMAGE_TAG:-gdpr}
+
+bs_docker() {
+  docker pull sesam/sesam-solr:"$SOLR_DOCKER_IMAGE_TAG"
+  docker pull sesam/sesam-redis:latest
+  docker pull sesam/databrowser:"$DATABROWSER_DOCKER_IMAGE_TAG"
+  docker pull v2tec/watchtower:latest
+}
+
+bs_watchtower() {
+  docker run -d \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /root/.docker/config.json:/config.json \
+  v2tec/watchtower redis solr databrowser
+}
+
+bs_solr() {
+  mkdir -p /srv/data/solr/{data,conf}
+  chown 1000:1000 /srv/data/solr/data
+
+  docker run -d --restart always --name solr \
+    --network=sesam \
+    -u "1000" \
+    -p 8983:8983 \
+    -v /srv/data/solr/data:/sesam/data/solr-data \
+    index.docker.io/sesam/sesam-solr:"$SOLR_DOCKER_IMAGE_TAG"
+
+  docker network connect microservices solr
+}
+
+bs_redis() {
+  mkdir -p /srv/data/redis/{data,conf,logs,temp}
+  chown -R 200:200 /srv/data/redis
+
+  envsubst < redis.conf > /srv/data/redis/conf/redis.conf
+
+  docker run -d --restart always --name redis \
+  --network=sesam \
+  -p 6379:6379 \
+  -v /srv/data/redis/data:/sesam/data:rw \
+  -v /srv/data/redis/conf:/sesam/conf:rw \
+  -v /srv/data/redis/logs:/sesam/logs:rw \
+  -v /srv/data/redis/temp:/sesam/temp:rw \
+  index.docker.io/sesam/sesam-redis:latest
+
+  docker network connect microservices redis
+}
+
+bs_databrowser() {
+  mkdir -p /srv/data/databrowser/{data,conf,logs}
+  chown -R 200:200 /srv/data/databrowser
+  envsubst < production.ini > /srv/data/databrowser/conf/production.ini
+
+  envsubst < databrowser.ini > /srv/data/databrowser/conf/databrowser.ini
+  cp resultitemrenderers.yaml /srv/data/databrowser/conf/resultitemrenderers.yaml
+
+  docker run -d --restart always --name databrowser \
+    --network=sesam \
+    -p 6543:6543 \
+    -v /srv/data/databrowser/conf:/sesam/conf \
+    -v /srv/data/databrowser/logs:/sesam/logs \
+    -v /srv/data/databrowser/data:/sesam/data \
+    -e SESAM_CONF=/sesam/conf \
+    -e SESAM_LOGS=/sesam/logs \
+    -e SESAM_DATA=/sesam/data \
+    index.docker.io/sesam/databrowser:"$DATABROWSER_DOCKER_IMAGE_TAG" pserve --reload /sesam/conf/production.ini
+
+  docker network connect microservices databrowser
+}
 
 echo I will use the following settings:
 echo SUBSCRIPTION_ID: \""$SUBSCRIPTION_ID"\".
@@ -27,90 +95,16 @@ echo SOLR_DOCKER_IMAGE_TAG: \""$SOLR_DOCKER_IMAGE_TAG"\".
 
 read -p "Do these settings look ok? (y/n)" -n 1 -r
 echo    # (optional) move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
   echo "Starting to create/update the various docker containers..."
   set -x
 
-  bs_docker() {
-    docker pull sesam/sesam-solr:"$SOLR_DOCKER_IMAGE_TAG"
-    docker pull sesam/sesam-redis:latest
-    docker pull sesam/databrowser:"$DATABROWSER_DOCKER_IMAGE_TAG"
-    docker pull v2tec/watchtower:latest
-    #docker logout
-    #rm -rf ~/.docker
-  }
-
-  bs_watchtower() {
-    docker run -d \
-    --name watchtower \
-    -v /var/run/docker.sock:/var/run/docker.sock \
-    -v /root/.docker/config.json:/config.json \
-    v2tec/watchtower redis solr databrowser
-  }
-
-  bs_solr() {
-    mkdir -p /srv/data/solr/{data,conf}
-    chown 1000:1000 /srv/data/solr/data
-
-    docker run -d --restart always --name solr \
-      --network=sesam \
-      -u "1000" \
-      -p 8983:8983 \
-      -v /srv/data/solr/data:/sesam/data/solr-data \
-      index.docker.io/sesam/sesam-solr:"$SOLR_DOCKER_IMAGE_TAG"
-
-    docker network connect microservices solr
-  }
-
-  bs_redis() {
-    mkdir -p /srv/data/redis/{data,conf,logs,temp}
-    chown -R 200:200 /srv/data/redis
-
-    envsubst < redis.conf > /srv/data/redis/conf/redis.conf
-
-    docker run -d --restart always --name redis \
-    --network=sesam \
-    -p 6379:6379 \
-    -v /srv/data/redis/data:/sesam/data:rw \
-    -v /srv/data/redis/conf:/sesam/conf:rw \
-    -v /srv/data/redis/logs:/sesam/logs:rw \
-    -v /srv/data/redis/temp:/sesam/temp:rw \
-    index.docker.io/sesam/sesam-redis:latest
-
-    docker network connect microservices redis
-  }
-
-  bs_databrowser() {
-    mkdir -p /srv/data/databrowser/{data,conf,logs}
-    chown -R 200:200 /srv/data/databrowser
-    envsubst < production.ini > /srv/data/databrowser/conf/production.ini
-
-    envsubst < databrowser.ini > /srv/data/databrowser/conf/databrowser.ini
-    cp resultitemrenderers.yaml /srv/data/databrowser/conf/resultitemrenderers.yaml
-
-    docker run -d --restart always --name databrowser \
-      --network=sesam \
-      -p 6543:6543 \
-      -v /srv/data/databrowser/conf:/sesam/conf \
-      -v /srv/data/databrowser/logs:/sesam/logs \
-      -v /srv/data/databrowser/data:/sesam/data \
-      -e SESAM_CONF=/sesam/conf \
-      -e SESAM_LOGS=/sesam/logs \
-      -e SESAM_DATA=/sesam/data \
-      index.docker.io/sesam/databrowser:"$DATABROWSER_DOCKER_IMAGE_TAG" pserve --reload /sesam/conf/production.ini
-
-    docker network connect microservices databrowser
-  }
-
-  if [ "$SKIP_PULL" != "1" ];
-  then
+  if [ "$SKIP_PULL" != "1" ]; then
     bs_docker
   fi
 
   set +e
-  docker stop solr redis databrowser watchtower
-  docker rm solr redis databrowser watchtower
+  docker stop solr redis databrowser watchtower | xargs -r docker rm
   set -e
 
   bs_solr
